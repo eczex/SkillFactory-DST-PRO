e797b1bc2a092d89b078e0747859f060dc2b3696
Mitya <skillfactory7788@mitya.im>
2020-07-26 22:50:11 +0300

    added unit-0 project-0

---
+++ unit-0/project-0.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def game_core_v3(number):
+    count = 1
+
+    predict = 50
+    half = predict // 2
+    while number != predict:
+        count+=1
+        if number > predict: 
+            predict += half
+        elif number < predict: 
+            predict -= half
+        half = half // 2 or 1
+    return(count) # выход из цикла, если угадали
+        
+        
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
+
+score_game(game_core_v3)
